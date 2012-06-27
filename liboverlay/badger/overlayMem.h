a663fc409da66163228f42a461e8f994db1f3f55
Amara Venkata Mastan Manoj Kumar <manojavm@codeaurora.org>
2012-06-26 20:04:58 -0700

    liboverlay: Remove GRALLOC_USAGE_PRIVATE_DO_NOT_MAP flag for             Rotator Memory
    When we allocate Rotator Memory from IO_MMU memory, there are chances
    of getting uncleared memory. This is taken care by removing if DO_NOT_MAP
    flag is removed.
    
    Change-Id: I2f6314d4f2de62a0ccedc7250c9a43f0c8f8d1af
    CRs-Fixed: 364812

--- liboverlay/badger/overlayMem.h
+++ liboverlay/badger/overlayMem.h
@@ -118,8 +118,7 @@ inline bool OvMem::open(uint32_t numbufs,
         uint32_t bufSz, int flags)
 {
     alloc_data data;
-    int allocFlags = GRALLOC_USAGE_PRIVATE_IOMMU_HEAP |
-            GRALLOC_USAGE_PRIVATE_DO_NOT_MAP;
+    int allocFlags = GRALLOC_USAGE_PRIVATE_IOMMU_HEAP;
     int err = 0;
 
     OVASSERT(numbufs && bufSz, "numbufs=%d bufSz=%d", numbufs, bufSz);
