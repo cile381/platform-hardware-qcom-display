mozilla-b2g/hardware_qcom_display

qcom/display: add perform function to fb/hwc HALS
- Add perform function to framebuffer and HWComposer HALS
- This will be used communicate custom events(external display,
  video overlay, orientation, etc.,) to HALS
- remove setActionsafe API(not used)

Change-Id: I398b5c179ee68c88fd5aca4da6743285068f7a7a

libhwcomposer/external_display_only.h
@@ -268,7 +268,7 @@ inline void ExtDispOnly::startDefaultMirror(hwc_context_t* ctx) {
     framebuffer_device_t *fbDev = hwcModule->fbDevice;
     if (fbDev) {
         //mHDMIEnabled could be HDMI/WFD/NO EXTERNAL
-        fbDev->enableHDMIOutput(fbDev, ctx->mHDMIEnabled);
+        fbDev->perform(fbDev, EVENT_EXTERNAL_DISPLAY, ctx->mHDMIEnabled);
     }
 #endif
 }
@@ -280,7 +280,7 @@ inline void ExtDispOnly::stopDefaultMirror(hwc_context_t* ctx) {
         reinterpret_cast<private_hwc_module_t*>(dev->common.module);
     framebuffer_device_t *fbDev = hwcModule->fbDevice;
     if (fbDev) {
-        fbDev->enableHDMIOutput(fbDev, EXT_TYPE_NONE);
+        fbDev->perform(fbDev, EVENT_EXTERNAL_DISPLAY, EXT_TYPE_NONE);
     }
 #endif
 }

cile381/platform-hardware-qcom-display

CRs-Fixed: 341947
(cherry picked from commit 31999628ddd4a6c0996e22e9fbf1f311d6efb160)
Change-Id: Icc7c2b49fe53684a4550cbf65725edffcccf5bef

libhwcomposer/external_display_only.h
@@ -268,7 +268,7 @@ inline void ExtDispOnly::startDefaultMirror(hwc_context_t* ctx) {
     framebuffer_device_t *fbDev = hwcModule->fbDevice;
     if (fbDev) {
         //mHDMIEnabled could be HDMI/WFD/NO EXTERNAL
-        fbDev->enableHDMIOutput(fbDev, ctx->mHDMIEnabled);
+        fbDev->perform(fbDev, EVENT_EXTERNAL_DISPLAY, ctx->mHDMIEnabled);
     }
 #endif
 }
@@ -280,7 +280,7 @@ inline void ExtDispOnly::stopDefaultMirror(hwc_context_t* ctx) {
         reinterpret_cast<private_hwc_module_t*>(dev->common.module);
     framebuffer_device_t *fbDev = hwcModule->fbDevice;
     if (fbDev) {
-        fbDev->enableHDMIOutput(fbDev, EXT_DISPLAY_OFF);
+        fbDev->perform(fbDev, EVENT_EXTERNAL_DISPLAY, EXT_DISPLAY_OFF);
     }
 #endif
 }