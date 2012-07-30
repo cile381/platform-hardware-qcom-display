958474718000859faa3954ed0a6e12cc033f55e8
Saurabh Shah <saurshah@codeaurora.org>
2012-07-30 11:54:05 -0700

    liboverlay: Add floating pipe support for 4 layer MDP Composition
    --Floating pipe is the rgb0 pipe that could either be used for framebuffer
      or as one of the pipes for MDP composition.
    --Change state transition, so that FloatingPipe is never closed.
      Default transition now closes other pipes and copies Floating pipe.
    --Remove DEST_ALL usage from clients.
    --Remove the requirement to pass multiple pipeargs, even though only one is
      used.
    --Enable the src-mem-is-fb flag for MDP (rotator already has this)
    
    Change-Id: Ief397341600705736273bbb84022b5ac75335b75

--- liboverlay/overlayCtrlData.h
+++ liboverlay/overlayCtrlData.h
@@ -116,6 +116,9 @@ public:
     /* get overlay id in the mdp struct */
     int getPipeId() const;
 
+    /* sets flags specified by user */
+    void setFlags(int mdpFlags);
+
     /* queue buffer to the overlay */
     bool queueBuffer(int fd, uint32_t offset);
 
@@ -194,6 +197,10 @@ inline Data::~Data() { close(); }
 inline void Data::setPipeId(int id) { mMdp.setPipeId(id); }
 
 inline int Data::getPipeId() const { return mMdp.getPipeId(); }
+
+inline void Data::setFlags(int mdpFlags) {
+    mMdp.setFlags(mdpFlags);
+}
 
 inline bool Data::init(uint32_t fbnum) {
     if(!mMdp.init(fbnum)) {
