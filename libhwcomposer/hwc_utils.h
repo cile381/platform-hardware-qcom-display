d33233918c4cbc6b47218a5079da8cb7a17af0de
Arun Kumar K.R <akumarkr@codeaurora.org>
2012-08-20 13:00:46 -0700

    hwcomposer: commit for ext display in different thread
    - calling commit in a different thread so that the compositor thread
      will not be blocked and get better fps for UI on HDMI.
    - From hwc_set, after wait4Fbpost draw to UIMirror pipe.
    - Signal the commitExtDisplay thread which does the commit for the
      external display.
    - commit will wait for dmae done.
    - hwc_set will wait for commit to finish while video playback
    - increase the number of rotator buffers to 3
    
    Change-Id: Ib8c711512582281407855cf8004144f6a37822f9

--- libhwcomposer/hwc_utils.h
+++ libhwcomposer/hwc_utils.h
@@ -108,6 +108,12 @@ void wait4fbPost(hwc_context_t* ctx);
 // Waits for the fb_post to finish PAN (primary commit)
 void wait4Pan(hwc_context_t* ctx);
 
+// Waits for signal from hwc_set to commit on ext display
+void wait4CommitSignal(hwc_context_t* ctx);
+
+// Waits for the commit to finish on ext display
+void wait4ExtCommitDone(hwc_context_t* ctx);
+
 // Inline utility functions
 static inline bool isSkipLayer(const hwc_layer_t* l) {
     return (UNLIKELY(l && (l->flags & HWC_SKIP_LAYER)));
@@ -207,6 +213,16 @@ struct hwc_context_t {
     // Post-processing parameters
     overlay::PPMetaData mPpParams[PP_MAX_VG_PIPES];
 
+    // used for signalling the commit Ext Disp thread
+    bool mExtCommit;
+    pthread_mutex_t mExtCommitLock;
+    pthread_cond_t mExtCommitCond;
+
+    // used for signalling the composition thread
+    // from the       extDispCommit thread
+    bool mExtCommitDone;
+    pthread_mutex_t mExtCommitDoneLock;
+    pthread_cond_t mExtCommitDoneCond;
 };
 
 #endif //HWC_UTILS_H
