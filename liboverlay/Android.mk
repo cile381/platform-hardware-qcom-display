b52c946621cd524cebc2575f4fe6af3256c21ed7
Prabhanjan Kandula <pkandula@codeaurora.org>
2012-10-01 16:40:55 +0530

    liboverlay: Use camera heap for targets without mm heap.
    for 7x30 MM heap is not available. Use camera heap instead.
    
    CRs-fixed: 407303
    Change-Id: Ie94937fb59c40b49d7f5bb30d12b4ddea497a6fc

--- liboverlay/Android.mk
+++ liboverlay/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE                  := liboverlay
 LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdutils libmemalloc
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"overlay\"
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES := \
