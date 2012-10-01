b9296acdf4c6a73d2825ad319d89cb6e3ba20651
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2012-10-01 23:57:42 +0530

    display: gralloc: Add dependency on sanitized headers
    libmemalloc module sources using sanitized headers,
    so, adding dependency on sanitized headers to make sure
    the headers gets generated before compiling the sources.
    
    Change-Id: Ic9cad38bc690716c6c92116c7fbc6732449f6ddd
    (cherry picked from commit bc145da60ff66a6769a561de28d635861d6e7dfc)

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -30,10 +30,11 @@ include $(BUILD_SHARED_LIBRARY)
 
 #MemAlloc Library
 include $(CLEAR_VARS)
-LOCAL_MODULE           := libmemalloc
-LOCAL_MODULE_TAGS      := optional
-LOCAL_C_INCLUDES       := $(common_includes) $(kernel_includes)
-LOCAL_SHARED_LIBRARIES := $(common_libs) libgenlock libqdutils
-LOCAL_CFLAGS           := $(common_flags) -DLOG_TAG=\"memalloc\"
-LOCAL_SRC_FILES        :=  ionalloc.cpp alloc_controller.cpp
+LOCAL_MODULE                  := libmemalloc
+LOCAL_MODULE_TAGS             := optional
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libgenlock libqdutils
+LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"memalloc\"
+LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_SRC_FILES               :=  ionalloc.cpp alloc_controller.cpp
 include $(BUILD_SHARED_LIBRARY)
