e49a8c2ba65d950c87acbfed4682557858863e16
Naseer Ahmed <naseer@codeaurora.org>
2012-12-06 19:29:29 -0500

    external: memset commit struct
    Change-Id: I5e6d046f2fdff54fdd58aaca583c0ae8038a9a63

--- libexternal/external.cpp
+++ libexternal/external.cpp
@@ -449,7 +449,8 @@ bool ExternalDisplay::post()
         return false;
 
     struct mdp_display_commit ext_commit;
-    ext_commit.flags |= MDP_DISPLAY_COMMIT_OVERLAY;
+    memset(&ext_commit, 0, sizeof(struct mdp_display_commit));
+    ext_commit.flags = MDP_DISPLAY_COMMIT_OVERLAY;
     if (ioctl(mFd, MSMFB_DISPLAY_COMMIT, &ext_commit) == -1) {
          ALOGE("%s: MSMFB_DISPLAY_COMMIT for external failed, str: %s",
                                                 __FUNCTION__, strerror(errno));
