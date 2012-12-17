6f42549c8b36b4ec681e3d34d50450b073620687
Prabhanjan Kandula <pkandula@codeaurora.org>
2012-12-17 22:21:07 +0530

    libgralloc: Clean up Widevine flags and usage.
    Remove extra flag we have added in gralloc
    for content protection. Use system propety
    to fall back to level3 protection.
    
    CRs-fixed: 428647
    CRs-fixed: 424769
    CRs-fixed: 421767
    
    Change-Id: Iafa933bbd30b4999afcc6244a580f5a40a320b4f
    
    Conflicts:
    
    	libgralloc/gpu.cpp
    	liboverlay/overlayMem.h

--- libgralloc/gpu.cpp
+++ libgralloc/gpu.cpp
@@ -28,6 +28,8 @@
 #include "memalloc.h"
 #include "alloc_controller.h"
 
+#define SZ_1M 0x100000
+
 using namespace gralloc;
 
 gpu_context_t::gpu_context_t(const private_module_t* module,
@@ -136,6 +138,10 @@ int gpu_context_t::gralloc_alloc_buffer(size_t size, int usage,
         data.align = 8192;
     else
         data.align = getpagesize();
+
+    if (usage & GRALLOC_USAGE_PROTECTED) {
+        data.align = ALIGN(data.align, SZ_1M);
+    }
     data.pHandle = (unsigned int) pHandle;
     err = mAllocCtrl->allocate(data, usage);
 
@@ -220,8 +226,7 @@ int gpu_context_t::alloc_impl(int w, int h, int format, int usage,
     // All buffers marked as protected or for external
     // display need to go to overlay
     if ((usage & GRALLOC_USAGE_EXTERNAL_DISP) ||
-        (usage & GRALLOC_USAGE_PROTECTED) ||
-        (usage & GRALLOC_USAGE_PRIVATE_CP_BUFFER)) {
+        (usage & GRALLOC_USAGE_PROTECTED)) {
         bufferType = BUFFER_TYPE_VIDEO;
     }
     int err;
