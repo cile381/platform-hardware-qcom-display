46a4d60e7e007de290421e602706d4557adcdf5c
Naseer Ahmed <naseer@codeaurora.org>
2013-04-29 15:47:47 -0400

    gralloc: Add ITU-R standard info for YUV formats
    These format qualifiers will help graphics decide which
    standard to use when working with a YUV texture.
    
    Change-Id: I6ad464e6b070d3b375c5e07f03228308d15407cd

--- libgralloc/gralloc_priv.h
+++ libgralloc/gralloc_priv.h
@@ -157,6 +157,9 @@ struct private_handle_t : public native_handle {
             PRIV_FLAGS_VIDEO_ENCODER      = 0x00010000,
             PRIV_FLAGS_CAMERA_WRITE       = 0x00020000,
             PRIV_FLAGS_CAMERA_READ        = 0x00040000,
+            PRIV_FLAGS_ITU_R_601          = 0x00200000,
+            PRIV_FLAGS_ITU_R_601_FR       = 0x00400000,
+            PRIV_FLAGS_ITU_R_709          = 0x00800000,
         };
 
         // file-descriptors
