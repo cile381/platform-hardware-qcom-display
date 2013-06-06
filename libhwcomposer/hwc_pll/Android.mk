a2f084ae9b9c953c8a9b7b722081ebb171eb9d07
Ananda Kishore <kananda@codeaurora.org>
2013-06-06 18:32:24 +0530

    hwc: API to support the PLL changes
     API exposed from HWC to support the Dynamic PLL Change
    
    Change-Id: I752e6f856657d915dcfd89a6558c6855295aec2c

---
+++ libhwcomposer/hwc_pll/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+include $(LOCAL_PATH)/../../common.mk
+include $(CLEAR_VARS)
+#libhwcpllchange library
+include $(CLEAR_VARS)
+LOCAL_MODULE                  := libhwcpllchange
+LOCAL_MODULE_PATH             := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_TAGS             := optional
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes) \
+                                 $(TOP)/hardware/qcom/display/libhwcomposer
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libhwcservice libhwcexternal libbinder
+LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+LOCAL_SRC_FILES               := hwc_pllchange.cpp
+include $(BUILD_SHARED_LIBRARY)
