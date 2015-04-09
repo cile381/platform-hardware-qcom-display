a6b7e5f02f8fae53acac7f86f4e969813ad4a51b
Nitesh Gupta <niteshg@codeaurora.org>
2015-04-09 12:45:45 +0530

    libqdutils: Send ACK to partial update from PP via dynamic link
    Exposing setPartialUpdateState to send the acknowledgment from
    PP regarding the turning OFF of all features completely so that
    partial update can be enabled for next commit onwards. The call
    can be made by native daemons over the dynamic link.
    Change-Id: I00d64bf3bf2dcdf831c5aaac58db07b9e84a314f

--- libqdutils/display_config.cpp
+++ libqdutils/display_config.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2013-2014 The Linux Foundation. All rights reserved.
+* Copyright (c) 2013-2015 The Linux Foundation. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
@@ -160,5 +160,14 @@ int setSecondaryDisplayStatus(int dpy, uint32_t status) {
 extern "C" int refreshScreen() {
     int ret = 0;
     ret = screenRefresh();
+    return ret;
+}
+
+// ----------------------------------------------------------------------------
+// Native daemons needs to send enable partial update ack for PU to enable
+// ----------------------------------------------------------------------------
+extern "C" int setPartialUpdateState() {
+    int ret = 0;
+    ret = setPartialUpdate(IQService::ENABLE_PARTIAL_UPDATE);
     return ret;
 }
