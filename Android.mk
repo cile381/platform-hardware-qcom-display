537cd898874cb8508729b69b9bb29f0af83b2079
cile381 <cile381@gmail.com>
2017-10-10 18:15:36 +0200

    superxe-oreo

--- Android.mk
+++ Android.mk
@@ -1,22 +1,31 @@
-ifeq ($(call is-board-platform-in-list, thulium),true)
-    TARGET_USES_SDE = true
-else
-    TARGET_USES_SDE = false
-endif
-
-display-hals := libgralloc libcopybit liblight libmemtrack libqservice libqdutils
-
-ifeq ($(TARGET_USES_SDE), true)
-    sde-libs := displayengine/libs
-    display-hals += $(sde-libs)/utils $(sde-libs)/core $(sde-libs)/hwc
-else
-    display-hals += libgenlock libhwcomposer liboverlay libhdmi
-endif
-
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-else
-ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-endif
+# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
+ifneq ($(TARGET_BOARD_AUTO),true)
+  ifneq ($(filter msm8084 msm8x84,$(TARGET_BOARD_PLATFORM)),)
+    #This is for 8084 based platforms
+    include $(call all-named-subdir-makefiles,msm8084)
+  else
+    ifneq ($(filter msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
+      include $(call all-named-subdir-makefiles,msm8226)
+    else
+      ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+        include $(call all-named-subdir-makefiles,msm8960)
+      else
+        ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
+          include $(call all-named-subdir-makefiles,msm8994)
+        else
+          ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)
+            include $(call all-named-subdir-makefiles,msm8909)
+          else
+            ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
+              include $(call all-named-subdir-makefiles,msm8996)
+            else
+              ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
+                include $(call all-named-subdir-makefiles,msm8998)
+              endif
+            endif
+          endif
+        endif
+      endif
+    endif
+  endif
 endif
